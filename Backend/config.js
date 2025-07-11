30de9503bb78f128137d5f5d588d17d0fe695458
sksumair <mohammedsaifuddinsumair_shaik@srmap.edu.in>
2025-07-11 14:32:32 +0530

    ,

--- Backend/config.js
+++ Backend/config.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/CarPool")
+mongoose.connect("mongodb://127.0.0.1:27017/CarPool")
   .then(() => {
     console.log("connected!");
   })
